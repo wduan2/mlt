4fbf9f805770b0016d04ba8b3916745c619a6e41
Wenyu Duan <duanwenyu1988@gmail.com>
2018-06-07 23:32:32 -0500

    Add start/resume/pause toggle button.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import {render} from 'react-dom'
 import UI from './ui/UI'
 
 render(
-    <UI/>,
+    <UI width={10} height={100} initTopLeft={[0, 4]}/>,
     document.getElementById('mlt')
 );
--- src/ui/UI.js
+++ src/ui/UI.js
@@ -4,6 +4,11 @@ import Event from '../core/event'
 import {Observable} from 'rxjs/Rx'
 
 class UI extends React.Component {
+    state = {
+        nextStatus: 'Start',
+        paused: true
+    };
+
     constructor(props) {
         super(props);
         const {width, height, initTopLeft} = props;
@@ -15,45 +20,56 @@ class UI extends React.Component {
     }
 
     resume() {
-        this.keyOb.subscribe(e => this.stage.reduce(e));
-        this.gravityOb.subscribe(() => this.stage.reduce(Event.DOWN));
+        this.activeKeyOb = keyOb.subscribe(e => this.stage.reduce(e));
+        this.activeGravityOb = gravityOb.subscribe(() => this.stage.reduce(Event.DOWN));
+        this.setState({paused: false, nextStatus: 'Pause'});
+
+        // TODO: handle game over!
     }
 
     pause() {
-        this.keyOb.unsubscribe();
-        this.gravityOb.unsubscribe();
+        // for RxJs 5
+        this.activeKeyOb.unsubscribe();
+        this.activeGravityOb.unsubscribe();
+        this.setState({paused: true, nextStatus: 'Resume'});
     }
-
-    keyOb = Observable.fromEvent(document, 'keydown')
-        .debounceTime(100)
-        .map(keyEvent => {
-            let event = null;
-            if (keyEvent.key === 'ArrowDown') {
-                event = Event.DOWN
-            } else if (keyEvent.key === 'ArrowLeft') {
-                event = Event.LEFT
-            } else if (keyEvent.key === 'ArrowRight') {
-                event = Event.RIGHT
-            } else if (keyEvent.key === 'ArrowUp') {
-                event = Event.ROTATE
-            } else if (keyEvent.keyCode === 32) {
-                // space key event doesn't have key value
-                event = Event.FALL
-            }
-            return event;
-        })
-        .filter(event => !!event);
-
-    gravityOb = Observable.interval(200).timeInterval();
 
     render() {
         return (
             <div>
-                <button onClick={this.resume}>Start</button>
+                <button onClick={() => {
+                    if (this.state.paused) {
+                        this.resume();
+                    } else {
+                        this.pause();
+                    }
+                }}>{this.state.nextStatus}</button>
                 <div>container</div>
             </div>
         )
     }
 }
+
+const keyOb = Observable.fromEvent(document, 'keydown')
+    .debounceTime(100)
+    .map(keyEvent => {
+        let event = null;
+        if (keyEvent.key === 'ArrowDown') {
+            event = Event.DOWN
+        } else if (keyEvent.key === 'ArrowLeft') {
+            event = Event.LEFT
+        } else if (keyEvent.key === 'ArrowRight') {
+            event = Event.RIGHT
+        } else if (keyEvent.key === 'ArrowUp') {
+            event = Event.ROTATE
+        } else if (keyEvent.keyCode === 32) {
+            // space key event doesn't have key value
+            event = Event.FALL
+        }
+        return event;
+    })
+    .filter(event => !!event);
+
+const gravityOb = Observable.interval(800).timeInterval();
 
 export default UI;
